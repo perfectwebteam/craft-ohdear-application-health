224e93aaa3b10ea8ce73ddd72753a1bda5d6c461
Sander Potjer <github@sanderpotjer.nl>
2025-05-06 17:22:20 +0200

    Codestyle
    Signed-off-by: Sander Potjer <github@sanderpotjer.nl>

--- src/OhdearApplicationHealth.php
+++ src/OhdearApplicationHealth.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Oh Dear Application Health checker for Craft CMS
+ *
+ * @link      https://perfectwebteam.com
+ * @copyright Copyright (c) 2025 Perfect Web Team
+ */
 
 namespace perfectwebteam\ohdearapplicationhealth;
 
--- src/controllers/HealthController.php
+++ src/controllers/HealthController.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Oh Dear Application Health checker for Craft CMS
+ *
+ * @link      https://perfectwebteam.com
+ * @copyright Copyright (c) 2025 Perfect Web Team
+ */
 
 namespace perfectwebteam\ohdearapplicationhealth\controllers;
 
@@ -11,35 +17,35 @@ use yii\web\Response;
 
 class HealthController extends Controller
 {
-	protected array|int|bool $allowAnonymous = true;
-	private const CACHE_KEY = 'ohdear-health-check-json';
-	private const CACHE_DURATION = 3600;
+    protected array|int|bool $allowAnonymous = true;
+    private const CACHE_KEY = 'ohdear-health-check-json';
+    private const CACHE_DURATION = 3600;
 
-	public function actionCheck(): Response
-	{
-		$force = Craft::$app->request->getQueryParam('force');
+    public function actionCheck(): Response
+    {
+        $force = Craft::$app->request->getQueryParam('force');
 
-		if (!$force && ($cachedResult = Craft::$app->cache->get(self::CACHE_KEY))) {
-			return $this->asJson(json_decode($cachedResult, true));
-		}
+        if (!$force && ($cachedResult = Craft::$app->cache->get(self::CACHE_KEY))) {
+            return $this->asJson(json_decode($cachedResult, true));
+        }
 
-		$this->validateHealthCheckSecret();
+        $this->validateHealthCheckSecret();
 
-		$checkResults = OhdearApplicationHealth::getInstance()->healthCheck->runChecks();
+        $checkResults = OhdearApplicationHealth::getInstance()->healthCheck->runChecks();
 
-		$resultJson = $checkResults->toJson();
-		Craft::$app->cache->set(self::CACHE_KEY, $resultJson, self::CACHE_DURATION);
+        $resultJson = $checkResults->toJson();
+        Craft::$app->cache->set(self::CACHE_KEY, $resultJson, self::CACHE_DURATION);
 
-		return $this->asJson(json_decode($resultJson, true));
-	}
+        return $this->asJson(json_decode($resultJson, true));
+    }
 
-	private function validateHealthCheckSecret(): void
-	{
-		$expectedSecret = App::env('OH_DEAR_HEALTH_REPORT_SECRET');
-		$receivedSecret = Craft::$app->request->getHeaders()->get('oh-dear-health-check-secret');
+    private function validateHealthCheckSecret(): void
+    {
+        $expectedSecret = App::env('OH_DEAR_HEALTH_REPORT_SECRET');
+        $receivedSecret = Craft::$app->request->getHeaders()->get('oh-dear-health-check-secret');
 
-		if (!$expectedSecret || $receivedSecret !== $expectedSecret) {
-			throw new ForbiddenHttpException('Invalid or missing health check secret.');
-		}
-	}
+        if (!$expectedSecret || $receivedSecret !== $expectedSecret) {
+            throw new ForbiddenHttpException('Invalid or missing health check secret.');
+        }
+    }
 }
